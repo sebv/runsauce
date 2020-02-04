dfd1fb84c850b1621369acd637f18912e662a191
dpgraham <dan@saucelabs.com>
2020-02-04 11:51:39 -0800

    Add more python tests

--- scripts/opens-chrome-default.py
+++ scripts/opens-chrome-default.py
@@ -2,28 +2,26 @@ import unittest
 import os
 from appium import webdriver
 
-caps = {}
-#caps['appiumVersion'] = "1.15.0"
-caps['automationName'] = 'uiautomator2'
-caps['deviceName'] = "Android GoogleAPI Emulator" # "Android Emulator"
-caps['deviceOrientation'] = "portrait"
-caps['appium_version'] = {"appium-url":"https://github.com/appium/appium-build-store/releases/download/broken/appium-patched.zip"}
-caps['app'] = "http://appium.s3.amazonaws.com/ContactManager.apk"
-caps['platformVersion'] = "9.0"
-caps['platformName'] = "Android"
 SAUCE_USERNAME = os.environ['SAUCE_USERNAME']
 SAUCE_ACCESS_KEY = os.environ['SAUCE_ACCESS_KEY']
+caps = {}
+caps['appiumVersion'] = "1.16.0"
+caps['automationName'] = 'uiautomator2'
+caps['deviceName'] = "Android GoogleAPI Emulator"
+#caps['app'] = "http://appium.s3.amazonaws.com/ContactManager.apk"
+caps['app'] = "http://appium.s3.amazonaws.com/ContactManager9.apk"
+caps['deviceOrientation'] = "portrait"
+caps['platformVersion'] = "9.0"
+caps['platformName'] = "Android"
+caps['autoGrantPermissions'] = "true"
 #host = "http://admin:147a1148-a221-4e3b-a1df-987fd51c5eea@ondemand.staging.saucelabs.net/wd/hub" # Staging
 #host = ("http://%s:%s@ondemand.eu-central-1.saucelabs.com/wd/hub" % (SAUCE_USERNAME, SAUCE_ACCESS_KEY)) # Frankfurt
 host = ("http://%s:%s@ondemand.saucelabs.com/wd/hub" % (SAUCE_USERNAME, SAUCE_ACCESS_KEY)) # Prod
+#host = 'http://admin:0e779f56-385a-41be-a562-6f6908bf5acf@localhost:4444/wd/hub'
 driver = webdriver.Remote(host, caps)
 
-result = driver.execute_script('mobile: shell', {
-    'command': 'dumpsys',
-    'args': ['package', 'com.google.android.gms'],
-    'includeStderr': True,
-    'timeout': 5000
-})
+driver.execute_script('mobile: shell', {'command': 'am start -a android.intent.action.VIEW -d http://www.stackoverflow.com'})
+result = driver.page_source
 print(result)
 
 driver.quit()
--- scripts/set-location.py
+++ scripts/set-location.py
@@ -1,13 +1,16 @@
 import unittest
 import os
+import time
 from appium import webdriver
+from appium.webdriver.extensions.location import Location
+from selenium.common.exceptions import WebDriverException
 
 caps = {}
-#caps['appiumVersion'] = "1.15.0"
+caps['appiumVersion'] = "1.16.0"
+#caps['appium_version'] = {"appium-url":"https://github.com/appium/appium-build-store/releases/download/broken/appium-patched.zip"}
 caps['automationName'] = 'uiautomator2'
 caps['deviceName'] = "Android GoogleAPI Emulator" # "Android Emulator"
 caps['deviceOrientation'] = "portrait"
-caps['appium_version'] = {"appium-url":"https://github.com/appium/appium-build-store/releases/download/broken/appium-patched.zip"}
 caps['app'] = "http://appium.s3.amazonaws.com/ContactManager.apk"
 caps['platformVersion'] = "9.0"
 caps['platformName'] = "Android"
@@ -16,14 +19,9 @@ SAUCE_ACCESS_KEY = os.environ['SAUCE_ACCESS_KEY']
 #host = "http://admin:147a1148-a221-4e3b-a1df-987fd51c5eea@ondemand.staging.saucelabs.net/wd/hub" # Staging
 #host = ("http://%s:%s@ondemand.eu-central-1.saucelabs.com/wd/hub" % (SAUCE_USERNAME, SAUCE_ACCESS_KEY)) # Frankfurt
 host = ("http://%s:%s@ondemand.saucelabs.com/wd/hub" % (SAUCE_USERNAME, SAUCE_ACCESS_KEY)) # Prod
+#host = "http://localhost:4723/wd/hub"
 driver = webdriver.Remote(host, caps)
-
-result = driver.execute_script('mobile: shell', {
-    'command': 'dumpsys',
-    'args': ['package', 'com.google.android.gms'],
-    'includeStderr': True,
-    'timeout': 5000
-})
-print(result)
+result = driver.set_location(49, -123, 0)
+print(driver.location)
 
 driver.quit()
